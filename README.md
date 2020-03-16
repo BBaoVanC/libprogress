888cb6586260ab3e920944807448ff7e5683a069
BBaoVanC <bbaovanc@protonmail.com>
2020-03-15 20:02:31 -0500

    Add setup.py and README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# libprogress
+Clone of the libprogress package from https://github.com/BBaoVanC/NameGenerator. For PyPI. Contains the latest release of libprogress.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libprogress",
+    version="3.0.1",
+    author="BBaoVanC",
+    author_email="bbaovanc@protonmail.com",
+    description="Library containing progress bar generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BBaoVanC/libprogress",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
